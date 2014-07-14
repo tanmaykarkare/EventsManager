bb8a6fbd508797c55845cca8ecc1da6943c665f3
Linesh <linesh.mali@gmail.com>
2014-07-14 22:42:09 +0100

    2nd Commit

---
+++ resource-bundles/Angular.resource/angular-seed-master/app/js/controllers.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', [])
+  .controller('MyCtrl1', ['$scope', function($scope) {
+
+  }])
+  .controller('MyCtrl2', ['$scope', function($scope) {
+
+  }])
+  .controller('MyCtrl3', ['$scope', function($scope) {
+
+  }]);
